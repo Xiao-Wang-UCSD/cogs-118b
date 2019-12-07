0be470d0f14a358bec9d371767d760d4cc19d5e8
Xiao Wang <xiw304@ucsd.edu>
2019-12-07 11:56:17 -0800

    good

--- face_2/CNN_Facial_Recognition-master/results/acc_loss_plot.m
+++ face_2/CNN_Facial_Recognition-master/results/acc_loss_plot.m
@@ -4,8 +4,7 @@ fontsize=15;
 linewidthval=2;
 plotcolor='b';
 plotcolor_nodropout='r';
-dirname='/home/ben/Dropbox/python/facial_rec_tensorflow_convnet/results/';
-websitedir='/home/ben/Dropbox/Ben_Suutari_Website/mysite/personal/static/personal/img/';
+dirname='./results/';
 
 accuracy=csvread(strcat(dirname,'accuracy'));
 loss=csvread(strcat(dirname,'loss'));
@@ -21,12 +20,10 @@ xlabel('Epoch #','FontSize',fontsize);
 ylabel('Test Accuracy (%)','FontSize',fontsize);
 ylim([0 100]);
 saveas(gcf,strcat(dirname,'CNNaccuracy_graph'),'png');
-saveas(gcf,strcat(websitedir,'CNNaccuracy_graph'),'png');
 plot(accuracy_nodropout*100,plotcolor_nodropout,'LineWidth',3);
 leg=legend('With Dropout','Without Dropout');
 set(leg,'box','off','FontSize',fontsize,'Location','SouthEast');
 saveas(gcf,strcat(dirname,'CNNaccuracy_graph_dropoutvsnodropout'),'png');
-saveas(gcf,strcat(websitedir,'CNNaccuracy_graph_dropoutvsnodropout'),'png');
 
 
 
@@ -37,15 +34,12 @@ axis square;
 xlabel('Epoch #','FontSize',fontsize);
 ylabel('Loss','FontSize',fontsize);
 saveas(gcf,strcat(dirname,'CNNloss_graph'),'png');
-saveas(gcf,strcat(websitedir,'CNNloss_graph'),'png');
 plot(loss,plotcolor,'LineWidth',3);hold on;
 saveas(gcf,strcat(dirname,'CNNloss_graph'),'png');
-saveas(gcf,strcat(websitedir,'CNNloss_graph'),'png');
 plot(loss_nodropout*100,plotcolor_nodropout,'LineWidth',3);
 leg=legend('With Dropout','Without Dropout');
 set(leg,'box','off','FontSize',fontsize);
 saveas(gcf,strcat(dirname,'CNNloss_graph_dropoutvsnodropout'),'png');
-saveas(gcf,strcat(websitedir,'CNNloss_graph_dropoutvsnodropout'),'png');
 
 figure;hold on;
 subplot(1,2,1);
@@ -64,7 +58,6 @@ xlabel('Epoch #','FontSize',fontsize);
 ylabel('Test Accuracy (%)','FontSize',fontsize);
 ylim([0 100]);
 saveas(gcf,strcat(dirname,'CNNloss_accuracy_graph'),'png');
-saveas(gcf,strcat(websitedir,'CNNloss_accuracy_graph'),'png');
 
 
 
